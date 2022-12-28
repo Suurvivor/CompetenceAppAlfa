b647fa2d32f3abe25a3df7c0562181543c077d4a
Suurvivor <fiiremage@gmail.com>
2022-12-28 23:38:42 +0100

    static folder fix

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use('/api/v1/planingtraining', protect, planingtraining);
 // });
 
 if (process.env.NODE_ENV === 'production') {
-   app.use(express.static('client/rfront/build'));
+   app.use(express.static('client/build'));
    app.get('*', (req, res) => res.sendFile(path.resolve(`${__dirname}`, 'client', 'build', 'index.html')));
 }
 
