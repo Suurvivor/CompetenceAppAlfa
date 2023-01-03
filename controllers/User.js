272cd7cd6d867dfd227822298cbb480be488bee4
Suurvivor <fiiremage@gmail.com>
2023-01-03 17:46:36 +0100

    fix

--- controllers/User.js
+++ controllers/User.js
@@ -35,7 +35,7 @@ exports.getUser = asyncHandler(async (req, res, next) => {
       return next(new ErrorResponse(`Could not find user of id ${req.params.id}`, 400));
    }
 
-   const planedTrainingForUsers = await PlaningTraining.find({ createdBy: userid })
+   const planedTrainingForUsers = await PlaningTraining.find({ createdBy: user._id })
       .populate({
          path: 'competenceId',
          select: 'name workplace',
@@ -52,7 +52,7 @@ exports.getUser = asyncHandler(async (req, res, next) => {
       })
       .sort({ trainingDate: 1 });
 
-   let planedTraining = await PlaningTraining.find({ trainedUserId: userid })
+   let planedTraining = await PlaningTraining.find({ trainedUserId: user._id })
       .populate({
          path: 'competenceId',
          select: 'name workplace',
